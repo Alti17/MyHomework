837a299ae37c5c0ce12ce35fc623290fa91c8bef
Romanyuk <sunrise15@mail.ru>
2022-06-20 17:17:44 +0400

    seminar4 Homework is done

---
+++ c#seminar4/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿void Degree(int a, int b)
+{
+    
+    if (b<=0)
+    {
+        if(b<0)
+        {
+            b = b*(-1);
+            Console.WriteLine("Результат:" + Math.Pow((a),b) + " При условии, что второе число преобразовано в натуральное.");
+        }
+        else
+        {
+            Console.WriteLine("Ноль не является натуральным числом.");
+        }
+    }
+    else
+    {
+        Console.WriteLine(Math.Pow((a),b));
+    }
+}
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Degree(a,b);
---
+++ c#seminar4/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿int size = 8;
+int[]array = new int [size];
+for(int i=0; i<size; i++)
+{
+    array[i] = new Random().Next();
+    Console.Write(array[i] + " ");
+}
