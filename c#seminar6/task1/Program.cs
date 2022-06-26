2aec83626e3257aef51c201ecfb25d1f069adabb
Romanyuk <sunrise15@mail.ru>
2022-06-26 19:33:14 +0400

    seminar6. HomeWork is done

---
+++ c#seminar6/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int CountPositive(string text, char a, char b, char c)
+{
+    int result = 0;
+    int length = text.Length;
+    for(int i = 0; i<length; i++)
+    {
+        if (text[i] == a) result=result+1;
+        if (text[i] == b) result=result-1; 
+        if (text[i] == c) result=result-1;
+    }
+    
+    result=result+1;
+    return result;
+}
+Console.WriteLine("Введите числа через пробел: ");
+int a = CountPositive(Console.ReadLine(), ' ', '-', '0');
+Console.WriteLine("Чисел больше нуля: " + a);
+
---
+++ c#seminar6/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] newArray = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        newArray[i] = new Random().Next(min, max+1);
+        Console.Write(newArray[i] + " ");
+    }
+    Console.Write("Это первый массив.");
+    Console.WriteLine();
+    return newArray;
+}
+
+int[] CreateRandomArray2(int size)
+{
+    int[] newArray = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        Console.Write(newArray[i] + " ");
+    }
+    Console.Write("Это второй массив.");
+    Console.WriteLine();
+    return newArray;
+}
+
+int[] CopyArray(int[] array1, int[] array2, int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        array2[i]=array1[i];
+        Console.Write(array2[i] + " ");
+    }
+    return array2;
+}
+int[] myArray = CreateRandomArray(5,100,1000);
+int[] otherArray = CreateRandomArray2(5);
+Console.WriteLine("Это первый массив во втором." + CopyArray(myArray, otherArray, 5));
