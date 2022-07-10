c91fb934ac0048bd7ac2c088dbe703dc1f2406a3
Romanyuk <sunrise15@mail.ru>
2022-07-10 22:42:14 +0400

    seminar 8. task1,task2 is done

---
+++ c#seminar8/task1/Program.cs
@@ -0,0 +1,53 @@
+﻿int [,] CreateRandomTwoDimArray(int a, int b, int min, int max)
+{
+    int [,] newMatrix=new int [a,b];
+    for(int i=0; i<a; i++)
+    {
+        for(int j=0; j<b; j++)
+        {
+            newMatrix[i,j]=new Random().Next(min,max+1);
+        }
+    }
+    return newMatrix;
+}
+
+void ShowArray(int[,]array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");   
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] SortingNumbers(int [,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1)-1; j++)
+        {
+            for(int m=j+1; m<array.GetLength(1); m++)
+            {
+                    if(array[i,j]>array[i,m])
+                    {
+                        int temp=array[i,j];
+                        array[i,j]=array[i,m];
+                        array[i,m]=temp;
+                    }
+                     
+            }
+        }
+        
+    }    
+    return array;
+}
+
+int [,]array=CreateRandomTwoDimArray(4,3,1,9);
+ShowArray(array);
+int [,]myArray=SortingNumbers(array);
+Console.WriteLine();
+ShowArray(myArray);
+
---
+++ c#seminar8/task2/Program.cs
@@ -0,0 +1,63 @@
+﻿int [,] CreateRandomTwoDimArray(int a, int b, int min, int max)
+{
+    int [,] newMatrix=new int [a,b];
+    for(int i=0; i<a; i++)
+    {
+        for(int j=0; j<b; j++)
+        {
+            newMatrix[i,j]=new Random().Next(min,max+1);
+        }
+    }
+    return newMatrix;
+}
+
+void ShowArray(int[,]array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");   
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] FindSumInArray(int [,] array)
+{
+    int sum=0;
+    int j=0;
+    int[] ArraySum=new int[array.GetLength(1)];
+    while (j<array.GetLength(1))
+    {
+        for(int i=0; i<array.GetLength(0); i++)
+        {
+            sum += array[i,j];
+        }
+        ArraySum[j]=sum;
+        Console.Write(ArraySum[j]+" ");
+        j++;
+        sum=0;
+    }
+    return ArraySum;
+}
+
+int FindMinSumInLine(int[] array, int min)
+{ 
+    
+    
+    for(int i=1; i<array.Length;i++)
+        if(array[i]<array[min])
+        {    
+            min=i;
+        }
+    return min;
+}
+
+int [,]array=CreateRandomTwoDimArray(2,3,1,9);
+ShowArray(array);
+Console.WriteLine();
+int [] myArray = FindSumInArray(array);
+int min = FindMinSumInLine(myArray, 0);
+Console.Write("Столбец с наименьшей суммой: " + min);
+Console.WriteLine();
