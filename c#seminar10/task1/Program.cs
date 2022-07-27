b35eea4c44d9b57ea312a403a32cbbc6d5bdc154
Romanyuk <sunrise15@mail.ru>
2022-07-28 01:19:51 +0400

    seminar10 Homework is done

---
+++ c#seminar10/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] CreateStringArray(int size)
+{
+    string[] words=new string[size];
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"Input {i+1} word: ");
+        words[i] = Console.ReadLine();
+    }
+    return words;
+}
+
+/*void ShowArray(string[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    Console.Write(array[i] + " ");
+    Console.WriteLine();
+}*/
+
+int NumberOfWordsWithFirstVowel(string[] array1, char[] array2)
+{
+    int count = 0;
+    for (int i=0; i<array1.Length; i++)
+        for (int k=0; k<array2.Length; k++)
+            if (array1[i][0] == array2[k])
+            count++;
+    return count;
+}
+
+Console.Write("Input number of words: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array1=CreateStringArray(size);
+char[] array2={'a','e','i','o','u','y'};
+Console.WriteLine("Number of words with first vowel is " + NumberOfWordsWithFirstVowel(array1, array2));
---
+++ c#seminar10/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] CreateStringArray(int size)
+{
+    string[] words=new string[size];
+    for(int i=0; i<size; i++)
+    {
+        Console.Write($"Input {i+1} word: ");
+        words[i] = Console.ReadLine();
+    }
+    return words;
+}
+
+void ShowArray(string[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+string[] ArrayCombination(string[] array, int size)
+{
+    string[] newarray=new string[size/2];
+    for (int i =0; i<size/2; i++)
+    newarray[i] = array[2*i] + array[2*i+1];
+    return newarray;
+}
+
+Console.Write("Input number of words: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array=CreateStringArray(size);
+ShowArray(array);
+string[] newarray = ArrayCombination(array, size);
+ShowArray(newarray);
